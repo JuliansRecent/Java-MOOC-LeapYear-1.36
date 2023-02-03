1a09aaec39a394a7e0a6b7287d161d0c0a018aae
JuliansRecent <juliansrecent@gmail.com>
2023-02-02 17:58:47 -0800

    Module complete. Create a terminal based application that takes a year and determines if it is a leap year or not using logical operators with if, else if, and else statements.

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -6,5 +6,18 @@ public class LeapYear {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Prompt the user for a year
+        System.out.println("Give a year:");
+        int year = Integer.valueOf(scan.nextLine());
+        // If the year is divisible by 100 AND 400, print "The year is a leap year."
+        if (year % 100 == 0 && year % 400 == 0) {
+            System.out.println("The year is a leap year.");
+        } else if (year % 4 == 0 && year % 100 !=0) {
+            // If the year is divisible by 4 only, print "The year is a leap year."
+            System.out.println("The year is a leap year.");
+        } else {
+            // If the year is not divisible by 4, print "The year is not a leap year."
+            System.out.println("The year is not a leap year.");
+        }
     }
 }
